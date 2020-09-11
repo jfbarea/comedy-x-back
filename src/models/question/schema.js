de5a71ef91a57c333d4407bb4184b2f2e034ba02
Fran Barea <jfcobarea@gmail.com>
2020-09-11 16:13:52 +0200

    changed schema

--- src/models/question/schema.js
+++ src/models/question/schema.js
@@ -2,8 +2,11 @@ const { Schema } = require('mongoose');
 
 const Question = new Schema({
   questionMessage: String,
+  shortQuestionMessage: String,
   answerMessage: String,
-  options: [{ id: Number, text: String, score: Number }]
+  options: [{ id: Number, text: String, score: Number }],
+  questionId: String,
+  selected: Boolean
 });
 
 module.exports = Question;
