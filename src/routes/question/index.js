1db5641a28191c98cb39b27591fba6e559ba17ad
Guillermo Garcia <g.gmenendez@pm.me>
2020-09-11 17:18:46 +0200

    PANIC BUTTON

--- src/routes/question/index.js
+++ src/routes/question/index.js
@@ -2,6 +2,7 @@ const get = require('./get');
 const post = require('./post');
 const list = require('./list');
 const select = require('./select');
+const reset = require('./reset');
 
 const BASE_URL = '/question';
 
@@ -9,5 +10,6 @@ module.exports = [
   { method: 'get', url: BASE_URL, handler: get },
   { method: 'get', url: `${BASE_URL}/all`, handler: list },
   { method: 'post', url: `${BASE_URL}/vote`, handler: post },
+  { method: 'post', url: `${BASE_URL}/reset`, handler: reset },
   { method: 'put', url: `${BASE_URL}/select`, handler: select },
 ]
---
+++ src/routes/question/reset.js
@@ -0,0 +1,18 @@
+const QuestionModel = require('../../models/question');
+
+const MANDATORY_HEADER = 'cmdx-admin';
+
+  module.exports = async (req, res) => {
+    if (req.headers[MANDATORY_HEADER]) {
+      const questions = await QuestionModel.find({});
+      await Promise.all(questions.map(async (q) => {
+        q.selected = q.id === 'q1';
+        q.options = q.options.map(a => ({ id: a.id, text: a.text, score: 0 }));
+        await q.save();
+      }));
+
+      res.send();
+    }
+
+    res.status(400).end();
+}
