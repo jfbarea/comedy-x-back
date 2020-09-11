25ac2ab374c6c5618aba3599b541863412a39646
Guillermo Garcia <g.gmenendez@pm.me>
2020-09-11 17:04:08 +0200

    Added question select

--- src/routes/question/index.js
+++ src/routes/question/index.js
@@ -1,11 +1,13 @@
 const get = require('./get');
 const post = require('./post');
 const list = require('./list');
+const select = require('./select');
 
 const BASE_URL = '/question';
 
 module.exports = [
   { method: 'get', url: BASE_URL, handler: get },
+  { method: 'get', url: `${BASE_URL}/all`, handler: list },
   { method: 'post', url: `${BASE_URL}/vote`, handler: post },
-  { method: 'get', url: `${BASE_URL}/all`, handler: list }
+  { method: 'put', url: `${BASE_URL}/select`, handler: select },
 ]
---
+++ src/routes/question/select.js
@@ -0,0 +1,21 @@
+const QuestionModel = require('../../models/question');
+
+const MANDATORY_HEADER = 'cmdx-admin';
+
+module.exports = async (req, res) => {
+  if (req.headers[MANDATORY_HEADER]) {
+    const { question } = req.body;
+    const active = await QuestionModel.findOne({ selected: true });
+    const next = await QuestionModel.findOne({ questionId: question, selected: false });
+
+    if (next && active.questionId !== next.questionId) {
+      active.selected = false;
+      await active.save();
+      next.selected = true;
+      await next.save();
+
+      res.send(next);
+    }
+  }
+  res.send();
+}
